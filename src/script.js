90dfcc92373dfec28764afeb398861c598373354
Alex Bakoushin <alex@bakoush.in>
2019-03-13 11:04:45 +0300

    Play with scale to change dimesions

--- src/script.js
+++ src/script.js
@@ -43,46 +43,94 @@ function normalizeData(data, normalizedInterval) {
 // const data = [10, 5, 30, 15, 25, 45, 17, 32];
 
 const slider = document.querySelector('.slider--frame');
-slider.onpointerdown = down_handler;
-slider.onpointermove = move_handler;
-slider.onpointerup = up_handler;
-slider.onpo;
+// slider.onpointerdown = down_handler;
+// slider.onpointermove = move_handler;
+// slider.onpointerup = up_handler;
 
+// let isMoving = false;
+// let pointerPosition = null;
+// let sliderPosition = 0;
+// let sliderWidth = slider.clientWidth;
+// let rootWidth = root.clientWidth;
+
+// function down_handler(e) {
+//   isMoving = true;
+//   pointerPosition = e.clientX;
+//   sliderWidth = slider.clientWidth;
+//   rootWidth = root.clientWidth;
+//   slider.setPointerCapture(e.pointerId);
+//   console.log('down', pointerPosition, sliderPosition);
+// }
+
+// function move_handler(e) {
+//   if (!isMoving) return;
+
+//   const diff = e.clientX - pointerPosition;
+//   sliderPosition += diff;
+
+//   if (sliderPosition < 0) sliderPosition = 0;
+//   if (sliderPosition + sliderWidth > rootWidth)
+//     sliderPosition = rootWidth - sliderWidth;
+
+//   requestAnimationFrame(() => {
+//     e.target.style.transform = `translateX(${sliderPosition}px)`;
+//   });
+//   console.log('move', pointerPosition, sliderPosition);
+
+//   pointerPosition = e.clientX;
+// }
+
+// function up_handler(e) {
+//   console.log('up');
+//   isMoving = false;
+//   pointerPosition = null;
+//   slider.releasePointerCapture(e.pointerId);
+// }
+
+// SLIDER LEFT RIGHT CONTROLS
 let isMoving = false;
 let pointerPosition = null;
 let sliderPosition = 0;
 let sliderWidth = slider.clientWidth;
 let rootWidth = root.clientWidth;
+let initialSliderWidth = sliderWidth;
 
-function down_handler(e) {
+const sliderControlRight = document.querySelector('.slider--control__right');
+sliderControlRight.onpointerdown = e => {
   isMoving = true;
   pointerPosition = e.clientX;
-  sliderWidth = slider.clientWidth;
+  sliderWidth = e.target.clientWidth;
   rootWidth = root.clientWidth;
-  slider.setPointerCapture(e.pointerId);
-  console.log('down', pointerPosition, sliderPosition);
-}
-
-function move_handler(e) {
+  e.target.setPointerCapture(e.pointerId);
+  console.log('left down', pointerPosition, sliderPosition);
+};
+sliderControlRight.onpointermove = e => {
   if (!isMoving) return;
 
   const diff = e.clientX - pointerPosition;
-  sliderPosition += diff;
+  sliderWidth += diff;
+  const scale = sliderWidth / initialSliderWidth;
+  // sliderPosition = diff;
 
-  if (sliderPosition < 0) sliderPosition = 0;
-  if (sliderPosition + sliderWidth > rootWidth)
-    sliderPosition = rootWidth - sliderWidth;
+  // if (sliderPosition < 0) sliderPosition = 0;
+  // if (sliderPosition + sliderWidth > rootWidth)
+  //   sliderPosition = rootWidth - sliderWidth;
 
-  slider.style.transform = `translateX(${sliderPosition}px)`;
+  requestAnimationFrame(() => {
+    slider.style.transformOrigin = 'left';
+    slider.style.transform = `scaleX(${scale})`;
+    //slider.style.width = `${sliderWidth}px`;
+    // slider.style.transform = `translateX(${sliderPosition}px)`;
+    console.log(slider.getBoundingClientRect().width);
+  });
 
-  console.log('move', pointerPosition, sliderPosition);
+  console.log('left move', pointerPosition, sliderPosition);
 
   pointerPosition = e.clientX;
-}
-
-function up_handler(e) {
-  console.log('up');
+};
+sliderControlRight.onpointerup = e => {
+  console.log('left up');
   isMoving = false;
   pointerPosition = null;
-  slider.releasePointerCapture(e.pointerId);
-}
+  e.target.releasePointerCapture(e.pointerId);
+};
