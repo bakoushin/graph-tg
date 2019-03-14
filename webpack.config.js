aafa81dcee3d22f8ff849c424eb570c69da65abf
Alex Bakoushin <alex@bakoush.in>
2019-03-14 11:16:16 +0300

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const outputDirectory = 'dist';
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, outputDirectory),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  devServer: {
+    port: 3000,
+    open: false
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+      //favicon: "./public/favicon.ico"
+    })
+  ]
+};
