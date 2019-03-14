7f72c09bd6f78f340e7552756fc9afe4b8793b58
Alex Bakoushin <alex@bakoush.in>
2019-03-14 17:23:33 +0300

    Add animation

--- src/Polyline/Polyline.js
+++ src/Polyline/Polyline.js
@@ -26,14 +26,43 @@ class Polyline {
     this._svg.appendChild(this._polyline);
   }
   update(points) {
+    if (this._rafId) {
+      cancelAnimationFrame(this._rafId);
+    }
+
     this._points = points;
     // animate change in points
-    requestAnimationFrame(() => {
-      this._points.forEach(([x, y], index) => {
-        this._polyline.points[index].x = x;
-        this._polyline.points[index].y = y;
-      });
+    const start = performance.now();
+    const duration = 1000;
+
+    const diff = this._points.map(([x, y], index) => {
+      const diffX = x - this._polyline.points[index].x;
+      const diffY = y - this._polyline.points[index].y;
+      return [diffX, diffY];
     });
+
+    const initialPoints = Array.from(this._polyline.points).map(({ x, y }) => {
+      return { x, y };
+    });
+
+    const animate = now => {
+      const elapsed = now - start;
+      const progress = Math.min(1, elapsed / duration);
+      diff.forEach(([x, y], index) => {
+        this._polyline.points[index].x = initialPoints[index].x + x * progress;
+        this._polyline.points[index].y = initialPoints[index].y + y * progress;
+      });
+      if (progress < 1) {
+        this._rafId = requestAnimationFrame(animate);
+      }
+    };
+    this._rafId = requestAnimationFrame(animate);
+    // requestAnimationFrame(() => {
+    //   this._points.forEach(([x, y], index) => {
+    //     this._polyline.points[index].x = x;
+    //     this._polyline.points[index].y = y;
+    //   });
+    // });
   }
   show() {
     //opacity +
