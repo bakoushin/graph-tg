3fe1dc27ac6b84b326222c48a91140ea6faae44b
Alex Bakoushin <alex@bakoush.in>
2019-03-20 21:54:35 +0300

    Remove forced HMR

--- src/index.js
+++ src/index.js
@@ -377,5 +377,3 @@ add.addEventListener('click', () => {
   // sparkline._data['y0'].data = [...data, ...sparkline._data['y0'].data];
   // sparkline.onResize();
 });
-
-if (module.hot) module.hot.accept();
