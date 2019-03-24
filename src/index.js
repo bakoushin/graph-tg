58e35def918334e9dc012a5133a8e5db2745bdb4
Alex Bakoushin <alex@bakoush.in>
2019-03-24 10:36:04 +0300

    Move data to src

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import Graph from './Graph/Graph';
 import './style.css';
 
 // TODO: get dataset from XHR
-import dataset from '../spec/chart_data.json';
+import dataset from './chart_data.json';
 
 const main = document.getElementById('main');
 // const charts = dataset.map(item => new Graph(main, item));
